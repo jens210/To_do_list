fe14f0df6be9f7a67e86eb3cf46253a2b4ed17fc
jens210 <jensvk94@gmail.com>
2017-05-01 11:34:23 +0200

    add() fixed
    add function does not add empty objects to the array.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,63 @@
 var todoItems = ["Thing", "Cake", "Bat", "Stuff"];
 
 // checks if clicked
-// lav knapper X remove
+// lav knapper X remove udfor hvert enkelt todoItems obj
 function doStuff(){
+
   for(var i = 0; i < todoItems.length; i++){
     todoItemStuff = "<li class='list_todo_items' onclick='checked(this)'>" +
-     todoItems[i] + "<button onclick='remove(" + i +")'>x</button></li>";
-
+     todoItems[i] + "<button onclick='remove(" + i +")'class='remove'>x</button></li>";
     document.getElementById('todo_list').innerHTML += todoItemStuff;
-
   }
+}
+
+function show(){
+  document.getElementById('todo_list').innerHTML = "";
+  var inputValue = form.todo_input.value;
+  doStuff();
+}
+
+function add(form){
+  var inputValue = form.todo_input.value;
+  if(inputValue === ""){
+  // nope
+  alert("Error write something! ");
+  }
+  else {
+  todoItems.push(inputValue);
+  document.getElementById('todo_list').innerHTML = "";
+  show();
+  }
+}
+
+// tager id fra doStuff().. todoItems[i] + "<button onclick='remove(" + i +")..
+// listener lytter på om knappen er clicked
+function remove(id){
+  todoItems.splice(id, 1);
+  var buttons = document.getElementsByClassName('remove');
+  for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', remove);
+  }
+  show();
+}
+
+function checked(tag){
+  tag.classList.toggle('active');
+}
+
+// clears form
+function clearThis(target){
+  target.value = "";
+}
+
+function sort(){
+  todoItems.sort();
+  document.getElementById("todo_list").innerHTML = "";
+  doStuff();
+}
+
+function reverse(){
+  todoItems.reverse();
+  document.getElementById("todo_list").innerHTML = "";
+  doStuff();
 }
